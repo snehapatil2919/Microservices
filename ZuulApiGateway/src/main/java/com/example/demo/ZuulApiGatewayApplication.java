a90398f89710d05e453ba797e6534ae752ef065a
snehapatil2919 <snehapatil2919@gmail.com>
2020-02-16 14:56:07 +0530

    Successfully Commited

---
+++ ZuulApiGateway/src/main/java/com/example/demo/ZuulApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+@SpringBootApplication
+@EnableZuulServer
+@EnableEurekaClient
+public class ZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGatewayApplication.class, args);
+	}
+
+}
