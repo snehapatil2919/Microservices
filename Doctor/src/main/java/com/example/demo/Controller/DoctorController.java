569871e7aeff2cb248edb616bb88c640b4cd0e8c
snehapatil2919 <snehapatil2919@gmail.com>
2020-02-18 12:31:57 +0530

    Successfully Commited

--- Doctor/src/main/java/com/example/demo/Controller/DoctorController.java
+++ Doctor/src/main/java/com/example/demo/Controller/DoctorController.java
@@ -1,20 +1,13 @@
 package com.example.demo.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.Model.Doctor;
-import com.example.demo.Service.DoctorService;
 
 @RestController
 public class DoctorController {
-	@Autowired
-	private DoctorService doctorService;
 
-	public ResponseEntity<Doctor> saveDoctorInfo(Doctor doctor) {
-		Doctor saveDoctor = doctorService.saveDoctor(doctor);
-		return new ResponseEntity<Doctor>(saveDoctor, HttpStatus.ACCEPTED);
+	@GetMapping("/doctor")
+	public String getDoctor() {
+		return "Hii these is a Doctor!....";
 	}
 }
